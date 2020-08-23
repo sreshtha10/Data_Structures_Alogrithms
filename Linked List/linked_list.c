4a06cdfb12bf3763316a050d746347e3249547f3
Sreshtha Mehrotra <55186163+sreshtha10@users.noreply.github.com>
2020-08-24 00:20:30 +0530

    Update linked_list.c

--- Linked List/linked_list.c
+++ Linked List/linked_list.c
@@ -14,6 +14,7 @@ void display(struct node *start)
 {
     struct node *p;
     p = start;
+    //If the start pointer points to nothing then list is empty.
     if (start == NULL)
     {
         printf("List is empty");
@@ -22,6 +23,7 @@ void display(struct node *start)
     while(p != NULL)
     {
         printf("%d",p->info);
+        //After accessing the first element, we use link part of the structure to access the next element.
         p = p->link;
     }
     
@@ -31,6 +33,7 @@ void display(struct node *start)
 
 struct node *add_at_beg(struct node*start,int value)
 {
+    //If we want to add a node at the beginning of linked list then we have start should point at this node and this node should point at the next node.
     struct node *tmp;
     tmp = (struct node *)malloc(sizeof(struct node));
     tmp->info = value;
@@ -57,7 +60,9 @@ void search(struct node *start,int val)
     printf("Element not found in the linked list");
 }//End of search function.
 
-struct node *add_at_end(struct node *start, int data)
+
+//function to add element at the end of linked list
+struct node *add_at_end(struct node *start, int data)// To add an element at the end of the linked list first we need to find the last element and then point the last element to the new node and new node to null.
 {
     struct node *tmp;
     struct node *p;
@@ -72,7 +77,9 @@ struct node *add_at_end(struct node *start, int data)
     p->link = tmp;
     return start;
     
-}
+}// end of the function
+
+
 
 int main()
 {
