4b34057a34bb5d55c95be61e91c0dded37dcb35b
Sreshtha Mehrotra <55186163+sreshtha10@users.noreply.github.com>
2020-09-15 19:20:03 +0530

    Update Circular_Linked_list.c

--- Linked List/Circular_Linked_list.c
+++ Linked List/Circular_Linked_list.c
@@ -9,7 +9,46 @@ typedef struct node{
     struct node* link;
 }Node;
 
+//Insert to empty list 
+void display(Node *last){
+    if(last == NULL){
+        printf("List is empty");
+        return;
+    }
+    Node *p;
+    p = last->link;
+    do{
+        printf("%d ",p->info);
+        p = p->link;
+    }while(p!= last->link);
+    printf("\n");
+    return;
+}
+
+
+
+Node * add_to_empty(Node *last,int data){
+    Node * tmp = (Node*)malloc(sizeof(Node));
+    tmp->info = data;
+    last = tmp;
+    tmp->link = last;
+    return last;
+}
+
+
+
+Node* add_to_beg(Node* last,int data){
+    Node *tmp = (Node*)malloc(sizeof(Node));
+    tmp->info = data;
+    tmp->link = last->link;
+    last->link= tmp;
+    return last;
+}
+
 int main(void){
     Node* last = NULL;
+    last = add_to_empty(last,1);
+    last = add_to_beg(last,2);
+    display(last);
     return 0;
 }
