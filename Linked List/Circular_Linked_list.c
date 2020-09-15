9d20ef35da9e496784035d426442ff72a68bd608
Sreshtha Mehrotra <55186163+sreshtha10@users.noreply.github.com>
2020-09-15 18:32:27 +0530

    Create Circular_Linked_list.c

---
+++ Linked List/Circular_Linked_list.c
@@ -0,0 +1,16 @@
+//Circular Linked List
+
+#include<stdio.h>
+#include<stdlib.h>
+
+
+typedef struct node{
+    struct node* prev;
+    int info;
+    struct node* next;
+}Node;
+
+int main(void){
+    Node* start = NULL;
+    return 0;
+}
