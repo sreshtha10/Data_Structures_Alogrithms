bd89f0a331b614fe3805d1215f8fb6e44b2dc4e6
Sreshtha Mehrotra <55186163+sreshtha10@users.noreply.github.com>
2020-09-19 16:22:26 +0530

    Update CircularLinkedList.java

--- Linked List/Java/CircularLinkedList.java
+++ Linked List/Java/CircularLinkedList.java
@@ -7,12 +7,35 @@ class CircularLinkedList{
         }
     }
     Node last = null;
-    
+    public void print(){
+        Node p;
+        if(last == null){
+            System.out.println("List is empty");
+            return;
+        }
+        p = last.next;
+        do{
+            System.out.println(p.data);
+        }while(p != last.next);
+        return;
+    }
+    public void insert(int data){
+        Node p;
+        Node tmp = new Node(data);
+        if(last == null){
+            last = tmp;
+            last.next = tmp;
+            return;
+        }
+        
+    }
 }
 
 public class Main{
     public static void main(String args[]){
         CircularLinkedList c = new CircularLinkedList();
+        c.insert(1);
+        c.print();
         
     }
 }
