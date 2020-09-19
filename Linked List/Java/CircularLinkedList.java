7d169be3982183fe0dbe553cca5664abb8028fb5
Sreshtha Mehrotra <55186163+sreshtha10@users.noreply.github.com>
2020-09-19 15:44:27 +0530

    Create CircularLinkedList.java

---
+++ Linked List/Java/CircularLinkedList.java
@@ -0,0 +1,18 @@
+class CircularLinkedList{
+    class Node{
+        int data;
+        Node next;
+        public Node(int data){
+            this.data = data;
+        }
+    }
+    Node last = null;
+    
+}
+
+public class Main{
+    public static void main(String args[]){
+        CircularLinkedList c = new CircularLinkedList();
+        
+    }
+}
