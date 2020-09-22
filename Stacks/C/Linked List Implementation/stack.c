c06bf52e9067c25284a2fe194c80d11d02f4ea84
Sreshtha Mehrotra <55186163+sreshtha10@users.noreply.github.com>
2020-09-22 18:48:03 +0530

    Update stack.c

--- Stacks/C/Linked List Implementation/stack.c
+++ Stacks/C/Linked List Implementation/stack.c
@@ -1,7 +1,16 @@
+// Linked List implementation
+
 #include<stdio.h>
 #include<stdlib.h>
 
-int main(void){
+typedef struct node{
+    int data;
+    struct node *next;
+}Node;
 
-      return 0;
+
+int main(void){
+    Node * start = NULL;
+    
+    return 0;
 }
