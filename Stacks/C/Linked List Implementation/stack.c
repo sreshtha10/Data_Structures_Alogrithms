97719c5445a30dfafb4b1515a7e78aa38fd0df58
Sreshtha Mehrotra <55186163+sreshtha10@users.noreply.github.com>
2020-09-22 18:59:12 +0530

    Update stack.c

--- Stacks/C/Linked List Implementation/stack.c
+++ Stacks/C/Linked List Implementation/stack.c
@@ -7,10 +7,56 @@ typedef struct node{
     int data;
     struct node *next;
 }Node;
+Node * top = NULL;
+
+void display(){
+    if(top == NULL){
+        printf("Stack is empty\n");
+        return;
+    }
+    Node *p = top;
+    while(p != NULL){
+        printf("%d ",p->data);
+        p = p->next;
+    }
+    printf("\n");
+    return;
+}
+
+void push(int x){
+    Node * tmp  = (Node*)malloc(sizeof(Node));
+    if(tmp == NULL){
+        printf("Stack Overflow\n");
+        return;
+    }
+    tmp->data = x;
+    tmp->next = top;
+    top = tmp;
+    return;
+}
+
+int pop(){
+    int x;
+    if(top == NULL){
+        printf("Stack Underflow\n");
+        return;
+    }
+    Node * tmp;
+    tmp = top;
+    x = top->data;
+    top = top->next;
+    free(tmp);
+    return x;
+}
+
 
 
 int main(void){
-    Node * start = NULL;
-    
+    display();
+    push(1);
+    push(2);
+    display();
+    printf("%d\n",pop());
+    display();
     return 0;
 }
