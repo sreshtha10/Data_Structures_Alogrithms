6706a80c0047c01f871dfb1de5fd773c35551e6d
Sreshtha Mehrotra <55186163+sreshtha10@users.noreply.github.com>
2020-09-22 18:39:17 +0530

    Update stacks.c

--- Stacks/C/Array Implementation/stacks.c
+++ Stacks/C/Array Implementation/stacks.c
@@ -1,5 +1,61 @@
+// Array implementation of Stack
 #include<stdio.h>
-int main(void){
+#include<stdio.h>
 
-  return 0;
+#define max 10   // Maximum size of our stack array
+int stack[max];  // array
+int top = -1;    // top represents the index of the top element, if stack is empty top = -1
+
+void display(){
+    if(top == -1){
+        printf("Stack is empty\n");
+        return;
+    }
+    for(int i = 0;i<=top;i++){
+        printf("%d ",stack[i]);
+    }
+    printf("\n");
+    return;
+}
+
+void push(int x){
+    if(top == 10){
+        printf("stack overflow\n");
+        return;
+    }
+    stack[top+1]= x;
+    top += 1;
+    return;
+}
+
+int pop(){
+    int item;
+    if(top == -1){
+        printf("Stack underflow\n");
+        return;
+    }
+    item = stack[top];
+    top -= 1;
+    return item;
+}
+
+int peek(){
+    int item;
+    if(top == -1){
+        printf("stack underflow\n");
+        return;
+    }
+    return stack[top];
+}
+
+int main(void){
+    
+    display();
+    push(1);
+    push(2);
+    push(3);
+    display();
+    printf("%d\n",pop());
+    printf("%d",peek());
+    
 }
