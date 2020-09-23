cf4565f4e6ac6cfe1298213e176556f0823acfee
Sreshtha Mehrotra <55186163+sreshtha10@users.noreply.github.com>
2020-09-23 23:44:38 +0530

    Update queue.c

--- Queue/C/Array Implementation/queue.c
+++ Queue/C/Array Implementation/queue.c
@@ -1,4 +1,46 @@
+// Array implementation of queue
+
 #include<stdio.h>
+#define MAX 10
+int q[MAX];
+int front  = -1,rear = -1;
+
+void insert(int x){
+    if(rear == MAX-1){
+        printf("Queue Overflow\n");
+        return;
+    }
+    if(front == -1)
+        front = 0;
+    rear = rear+1;
+    q[rear] = x;
+}
+
+int delete(){
+    int x;
+    if(front == -1 || front == rear +1){
+        printf("Queue underflow\n");
+        return;
+    }
+    x = q[front];
+    front  = front +1;
+    return x;
+}
+
+int peek(){
+    int x;
+    if(front == -1 || front  == rear+1){
+        printf("Queue Underflow\n");
+        return;
+    }
+    x = q[front];
+    return x;
+}
+
+void display(){
+    
+}
+
 int main(void){
-  return 0;
+    
 }
