fcfee68c2f04a6b51d0a7dede7fa645cc51ff4ff
Sreshtha Mehrotra <55186163+sreshtha10@users.noreply.github.com>
2020-09-25 00:46:14 +0530

    Update queue.java

--- Queue/Java/queue.java
+++ Queue/Java/queue.java
@@ -3,17 +3,45 @@ class Queue{
     class Node{
         int data;
         Node next;
-        public Queue(int data){
+        public Node(int data){
             this.data = data;
         }
     }
     Node front = null;
     Node rear = null;
+    public void display(){
+        Node p = front;
+        if(front == null){
+            System.out.println("Queue Underflow");
+            return;
+        }
+        while(p!=null){
+            System.out.println(p.data);
+            p = p.next;
+        }
+        return;
+    }
+    public void insert(int data){
+        Node tmp = new Node(data);
+        if(front == null){
+            front = tmp;
+        }
+        tmp.next = null;
+        if(rear != null){
+            rear.next= tmp;
+        }
+        rear = tmp;
+        return;
+    }
     
 }
 
 public class Main{
     public static void main(String args[]){
-        
+        Queue q = new Queue();
+        q.insert(1);
+        q.insert(2);
+        q.insert(3);
+        q.display();
     }
 }
