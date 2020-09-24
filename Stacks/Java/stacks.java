92fffe6c1dc0c924469fab811299a8368bdbf0cd
Sreshtha Mehrotra <55186163+sreshtha10@users.noreply.github.com>
2020-09-25 00:07:08 +0530

    Update stacks.java

--- Stacks/Java/stacks.java
+++ Stacks/Java/stacks.java
@@ -1,7 +1,53 @@
-// Stack 
+// Stack - LIFO (Last In First Out)
+class Stack {
+    class Node{
+        int data;
+        Node next;
+        public Node(int data){
+            this.data = data;
+        }
+    }
+    Node top = null;
+    public void display(){
+        if(top == null){
+            System.out.println("Stack Underflow");
+            return;
+        }
+        Node p = top;
+        while(p!= null){
+            System.out.println(p.data);
+            p = p.next;
+        }
+        return;
+    }
+    public void push(int data){
+        Node tmp = new Node(data);
+        if(top != null){
+            tmp.next = top;
+        }
+        else{
+            tmp.next = null;
+        }
+        top = tmp;
+        return;
+    }
+    
+    
+}
+
+
+
+
+
+
 
 public class Main{
     public static void main(String args[]){
-        return;
+        Stack stack = new Stack();
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+        stack.display();
+        
     }
 }
