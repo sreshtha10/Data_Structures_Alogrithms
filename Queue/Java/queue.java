64324831be75e120397beb5103718b650feb344e
Sreshtha Mehrotra <55186163+sreshtha10@users.noreply.github.com>
2020-09-25 00:18:58 +0530

    Update queue.java

--- Queue/Java/queue.java
+++ Queue/Java/queue.java
@@ -1,5 +1,19 @@
+// Queue FIFO - First In First Out 
+class Queue{
+    class Node{
+        int data;
+        Node next;
+        public Queue(int data){
+            this.data = data;
+        }
+    }
+    Node front = null;
+    Node rear = null;
+    
+}
+
 public class Main{
-  public static void main(String args[]){
-      return;
-      }
-  }
+    public static void main(String args[]){
+        
+    }
+}
