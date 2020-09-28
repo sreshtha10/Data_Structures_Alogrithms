7dd597f5affb03f9aa607e66f8ca56911fb03ef8
Sreshtha Mehrotra <55186163+sreshtha10@users.noreply.github.com>
2020-09-28 15:46:28 +0530

    Create Dequeue.c

---
+++ Queue/C/Dequeue.c
@@ -0,0 +1,17 @@
+/* Dequeue - Double Ended Queue
+    Functions : 1. insert at front end 
+                2. insert at rear end 
+                3. delete from front end
+                4. delete from rear end
+                */
+                
+#include<stdio.h>
+#define MAX 10
+int dq[MAX];
+int front =-1,rear = -1;
+
+void display(){
+    
+}
+
+
