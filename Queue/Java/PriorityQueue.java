e4143e64e85cf54e8b04432262fade7ce3d0b53a
Sreshtha Mehrotra <55186163+sreshtha10@users.noreply.github.com>
2020-09-28 22:37:44 +0530

    Update PriorityQueue.java

--- Queue/Java/PriorityQueue.java
+++ Queue/Java/PriorityQueue.java
@@ -1,6 +1,6 @@
 /* Priority Queue - Linked List Implementation.
-    It can be done in two ways : 1. Deletion at constant time and insertion at O(n)
-                                2. Insertion at constant time and Deletion at O(n)
+    It can be done in two ways : 1. Deletion at constant time and insertion at O(n) 
+                                2. Insertion at constant time and Deletion at O(n) 
             */
 class PriorityQueue{
     class Node{
@@ -14,6 +14,21 @@ class PriorityQueue{
         }
     }
     Node front = null;
+    public void display(){
+        if(front == null){
+            System.out.println("Queue Underflow");
+            return;
+        }
+        Node p = front;
+        while(p!= null){
+            System.out.println("Queue: "+p.data+" Priority: "+p.pr);
+            p = p.next;
+        }
+        return;
+    }
+    
+    
+    
 }
 
 
