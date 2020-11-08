81722218b799dd7ddebf709b6cb91b83ae380590
Sreshtha Mehrotra <55186163+sreshtha10@users.noreply.github.com>
2020-11-08 14:27:04 +0530

    Update tbt.c

--- TREES/C/Threaded Binary Tree/tbt.c
+++ TREES/C/Threaded Binary Tree/tbt.c
@@ -1,1 +1,11 @@
-// Threaded binary tree.
+/* threaded binary tree 
+For a binary tree, total links are 2n out of which only n-1 are utilized as it contain n-1 edges. So memory is wasted for n+1  NULL links. This space can be utilized to increase the 
+efficiency of binary tree. 
+In threaded binary tree, these n+1 NULL links will contains the address of their inorder predecessor(stored in left link) and inorder successor(stored in right link). These utilized links are called threads.
+*/
+#include<stdio.h>
+#include<stdlib.h>
+
+typedef struct treenode{
+    
+}
