5c50845906c222297eccdf9ac18e71547cdd87e3
Sreshtha Mehrotra <55186163+sreshtha10@users.noreply.github.com>
2021-06-12 20:25:46 +0530

    Create StoredEmployee.java

---
+++ Hash Tables/Java/Array Implementation + Linear Probing/StoredEmployee.java
@@ -0,0 +1,13 @@
+package project0;
+
+public class StoredEmployee {
+	
+	public String key;
+	public Employee employee;
+	
+	public StoredEmployee(String key, Employee employee) {
+		this.key = key;
+		this.employee = employee;
+	}
+	
+}
