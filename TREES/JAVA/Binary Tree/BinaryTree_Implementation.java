404b9fec9d034ae32da9508edc5d1967709c60ad
Sreshtha Mehrotra <55186163+sreshtha10@users.noreply.github.com>
2021-07-06 06:32:09 +0530

    Create BinaryTree_Implementation.java

---
+++ TREES/JAVA/Binary Tree/BinaryTree_Implementation.java
@@ -0,0 +1,34 @@
+package project0;
+
+class Node{
+	int key;
+  Node left;
+	Node right;
+	
+	Node(int key){
+		this.key = key;
+		left = right = null;
+	}
+}
+
+class BinaryTree{
+	
+	Node root;
+	
+	BinaryTree(Node root){
+		this.root = root;
+	}
+	
+	BinaryTree(){
+		this.root = null;
+	}
+}
+
+public class BinaryTree_Implementation {
+
+	public static void main(String args[]) {
+		
+		
+	}
+	
+}
