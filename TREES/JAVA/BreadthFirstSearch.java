c74d1a50ff6337f1524a4f04b421f865a5710fc1
Sreshtha Mehrotra <55186163+sreshtha10@users.noreply.github.com>
2021-07-11 18:12:35 +0530

    BFS for trees.

--- TREES/JAVA/BreadthFirstSearch.java
+++ TREES/JAVA/BreadthFirstSearch.java
@@ -6,7 +6,7 @@ class BreadthFirstSearch{
     this.tree = tree;
   }
   
-  public void BFS() {
+    public void BFS_traversal() {
 		Queue<Node> queue = new LinkedList<Node>(); 
 		if(this.tree.root == null) {
 			return;
@@ -27,6 +27,33 @@ class BreadthFirstSearch{
 			queue.remove();
 		}
 		
+	}
+	
+	public Node BFS(int key) {
+		Queue<Node> queue = new LinkedList<Node>(); 
+		if(this.tree.root == null) {
+			return null;
+		}
+		queue.add(this.tree.root);
+		
+		Node result = null;
+
+		while(!queue.isEmpty()) {
+			Node tmp = queue.peek();
+			if(tmp.left != null) {
+				queue.add(tmp.left);
+			}
+			if(tmp.right != null) {
+				queue.add(tmp.right);
+			}
+			
+			if(tmp.key == key) {
+				result = tmp;
+				break;
+			}
+			queue.remove();
+		}
+		return result;
 	}
 	
   
